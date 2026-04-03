f0db7d1c961edfe8d45fdeb0f5365b001d0d7e94
BHARGAV DUDANI <bhargavdudani@truesys.co.in>
2026-04-03 19:44:24 +0530

    fix

--- app/core/ml_engine.py
+++ app/core/ml_engine.py
@@ -3,8 +3,25 @@ import joblib
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
-model_path = os.path.join(BASE_DIR, "model", "model.pkl")
-vectorizer_path = os.path.join(BASE_DIR, "model", "vectorizer.pkl")
+model = None
+vectorizer = None
 
-model = joblib.load(model_path)
-vectorizer = joblib.load(vectorizer_path)
+def load_model():
+    global model, vectorizer
+
+    if model is None:
+        model_path = os.path.join(BASE_DIR, "model", "model.pkl")
+        vectorizer_path = os.path.join(BASE_DIR, "model", "vectorizer.pkl")
+
+        print("Loading model...")
+        model = joblib.load(model_path)
+        vectorizer = joblib.load(vectorizer_path)
+
+def ml_predict(text):
+    load_model()
+
+    X = vectorizer.transform([text])
+    pred = model.predict(X)[0]
+    prob = max(model.predict_proba(X)[0])
+
+    return pred, prob
